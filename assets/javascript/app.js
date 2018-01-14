c7e2a8b69364f7f9257083271b27570324fa2e20
katophelix <katharrismail@gmail.com>
2018-01-14 16:51:02 -0500

    quote api added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,19 @@
- $("myButton").onclick = function () {
-        location.href = "www.yoursite.com";
-    };
+var queryURL = "http://quotes.rest/qod.json";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      console.log(response);
+   
+var quote = response.contents.quotes[0].quote;
+$("#quote-links").append("Quote of the day: " + quote);
+
+
+var author = response.contents.quotes[0].author;
+$("#author-links").append(author);
+
+console.log(quote);
+console.log(author);
+
+});
